400fb42dce00514f4474fb87f046f302de1646e2
ibrahim mahmoud <embabyibrahim95@gmail.com>
2025-11-22 01:53:11 +0200

    display Ai chat in UI screen, disappear secrets key

---
+++ lib/core/services/git_it_services.dart
@@ -0,0 +1,23 @@
+import 'package:get_it/get_it.dart';
+import '../../features/authiciation/data/repo/auth_repo_impl.dart';
+import '../../features/authiciation/domain/repo/auth_repo.dart';
+import '../../features/home/data/repos/chat_repo_impl.dart';
+import '../../features/home/domain/chat_repo.dart';
+import '../../secrets.dart';
+import 'firebase_auth_services.dart';
+
+final getIt = GetIt.instance;
+
+void setupServices() {
+  getIt.registerSingleton<FirebaseAuthServices>(FirebaseAuthServices());
+
+  getIt.registerSingleton<AuthRepo>(
+    AuthRepoImpl(firebaseAuthServices: getIt<FirebaseAuthServices>()),
+  );
+
+  getIt.registerSingleton<ChatRepo>(
+    ChatRepoImpl(
+      Secrets.groqApiKey,
+    ),
+  );
+}
